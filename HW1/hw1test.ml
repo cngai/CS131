c036dca0ae9e6f64a69b442a3f15a66f890d420f
Christopher Tyler Ngai <ngai@lnxsrv09.seas.ucla.edu>
2019-01-16 16:46:27 -0800

    working on test cases

---
+++ HW1/hw1test.ml
@@ -0,0 +1,68 @@
+(*let my_subset_test0 = subset [] []
+let my_subset_test1 = subset [] [2;4]
+let my_subset_test2 = subset [1;1] [4;1]
+let my_subset_test3 = subset [1;1] [1]
+let my_subset_test4 = subset [1;2;3;4] [1;9;2;8;3;7;4;6]
+let my_subset_test5 = not (subset [1;5;4] [3;5;4;2;6])
+let my_subset_test6 = not (subset [1] [])
+
+let my_equal_sets_test0 = equal_sets [] []
+let my_equal_sets_test1 = equal_sets [1] [1]
+let my_equal_sets_test2 = equal_sets [1;4] [1;4]
+let my_equal_sets_test3 = equal_sets [1;2;3] [1;1;2;2;3;3]
+let my_equal_sets_test4 = equal_sets [1;1;2] [2;2;1]
+let my_equal_sets_test5 = not (equal_sets [1;2] [2;4])
+let my_equal_sets_test6 = not (equal_sets [3;5] [])
+
+let my_set_union_test0 = equal_sets (set_union [] []) []
+let my_set_union_test1 = equal_sets (set_union [] [1]) [1]
+let my_set_union_test2 = equal_sets (set_union [1] [2;3]) [1;2;3]
+let my_set_union_test3 = equal_sets (set_union [1;2] [2;3]) [1;2;3]
+let my_set_union_test4 = equal_sets (set_union [1;2;3;4] [5;6;7;8]) [1;2;3;4;5;6;7;8]
+let my_set_union_test5 = equal_sets (set_union [2;3;3;3;3] [5;3;3;1;]) [1;2;3;5]
+
+let my_set_intersection_test0 = equal_sets (set_intersection [] []) []
+let my_set_intersection_test1 = equal_sets (set_intersection [] [1]) []
+let my_set_intersection_test2 = equal_sets (set_intersection [1;2] [2;3]) [2]
+let my_set_intersection_test3 = equal_sets (set_intersection [1;1;2] [3;3;2;2;1]) [1;2]
+let my_set_intersection_test4 = equal_sets (set_intersection [1;2;3;4] [3;4;5;6]) [3;4]
+let my_set_intersection_test5 = equal_sets (set_intersection [1;2;2] [2;2;3]) [2]
+
+let my_set_diff_test0 = equal_sets (set_diff [] []) []
+let my_set_diff_test1 = equal_sets (set_diff [] [1]) []
+let my_set_diff_test2 = equal_sets (set_diff [2] []) [2]
+let my_set_diff_test3 = not (equal_sets (set_diff [] [3]) [3])
+let my_set_diff_test4 = equal_sets (set_diff [1;2;3;4] [3;4]) [1;2]
+let my_set_diff_test5 = equal_sets (set_diff [1;1;1;2;2;2] [2]) [1]
+
+let my_computed_fixed_point_test0 = computed_fixed_point (=) (fun x -> x / 5) 999999 = 0
+let my_computed_fixed_point_test1 = computed_fixed_point (=) (fun x -> x /. 0.5) 1. = infinity
+let my_computed_fixed_point_test2 = computed_fixed_point (=) (fun x -> x *. 5.) 1. = infinity
+let my_computed_fixed_point_test3 = computed_fixed_point (=) exp 1.= infinity
+let my_computed_fixed_point_test4 = computed_fixed_point (=) sqrt 100000. = 1.
+let my_computed_fixed_point_test5 = computed_fixed_point (=) (fun x -> x / 5) 999999 = 0
+let my_computed_fixed_point_test6 = computed_fixed_point (=) (fun x -> x *. 5.) (-1.) = neg_infinity *)
+
+type fr_nonterminals =
+  | One | Two | Three | Four | Five
+
+let fr_rules =
+   [One, [T"32"; N Two; T"29"];
+    One, [N Five; T"1"];
+    Two, [N Four; N Three; N One];
+    Two, [N Two];
+    Two, [N Two; N Three];
+    Three, [N Four; N Five];
+    Three, [T"-"; N One];
+    Four, [T"random1"];
+    Five, [T"random2"];
+    Five, [T"random3"];
+    Five, [T"random4"];
+    Five, [T"random5"];
+    Five, [T"random6"];
+    Five, [T"random7"];
+    Five, [T"random8"]]
+
+let fr_grammar = One, fr_rules
+
+let my_filter_reachable_test0 = filter_reachable fr_grammar
