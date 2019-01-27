d1f6e5511bad35abcecbb3e2e1f38d531b74088d
Christopher Tyler Ngai <ngai@lnxsrv09.seas.ucla.edu>
2019-01-26 22:09:23 -0800

    make matcher

--- HW2/hw2.ml
+++ HW2/hw2.ml
@@ -27,21 +27,73 @@ let is_leaf symbol =
 	| Leaf x -> true
 ;;
 
+(* takes in symbol ==> Leaf x, returns only x *)
+let delete_type symbol =
+    match symbol with
+    | Leaf x -> x
+ (*    | Node (x, y) -> x *)
+;;
+
 (* mutually recursive function that adds leaves to leaves_list *)
 let rec parse_tree_list tree leaves_list =
 	match tree with
-	| Leaf x -> (Leaf x) :: leaves_list
+	| Leaf x -> x :: leaves_list
 	| Node (x, y) -> iterate_list y leaves_list
 (* iterates through list of symbols if symbol is a Node *)
 and iterate_list l leaves_list =
 	match l with
 	| [] -> leaves_list
 	| h :: t -> 
-		if is_leaf h then h :: (iterate_list t leaves_list)
+		if is_leaf h then (delete_type h) :: (iterate_list t leaves_list)
 		else (parse_tree_list h leaves_list) @ (iterate_list t leaves_list)
 ;;
 
 (* traverses parse tree left to right and yields list of leaves encountered *)
-let rec parse_tree_leaves tree =
+let parse_tree_leaves tree =
 	parse_tree_list tree []
 ;;
+
+let match_term symbol accept frag =
+    match frag with
+    | [] -> None
+    | h :: t ->
+        if h = symbol then (accept t)
+        else None
+;;
+
+let rec append_matchers m1 matchers =
+    match matchers with
+    | [] -> m1
+    | h :: t -> (append_matchers (fun accept -> m1 (fun frag -> h accept frag)) t)
+;;
+
+(* non-terminal symbol matcher *)
+let rec match_nt pf ss al accept frag =
+    match al with
+    | [] -> None
+    | h1 :: t1 ->
+        let matcher = (
+            match h1 with
+            | [] -> None
+            | h2 :: t2 -> 
+                (append_matchers 
+                    (match_maker pf h2)
+                    (List.map (match_maker pf) t2)
+                accept frag
+                )
+        ) in
+        if matcher = None then match_nt pf ss t1 accept frag
+        else matcher
+and match_maker pf symbol =
+    match symbol with
+    | N x -> match_nt pf x (pf x)
+    | T x -> match_term x
+;;
+
+(* returns matcher for grammar gram *)
+let make_matcher gram =
+    match gram with
+    | (start_symb, prod_func) -> let alt_list = (prod_func start_symb) in
+        (fun accept frag -> match_nt prod_func start_symb alt_list accept frag)
+;;
+
--- HW2/hw2test.ml
+++ HW2/hw2test.ml
@@ -31,10 +31,7 @@ let my_convert_grammar_test0 = convert_grammar awksub_grammar
 let my_get_alt_list_test0 = get_alt_list awksub_rules Num 
 *)
 
-(* let parse_tree_2 =
-    (Node ("+", [Leaf 3; Node ("*", [Leaf 4; Leaf 5])])) *)
-
-let my_is_leaf_test0 = is_leaf (Leaf "L3")
+(* let my_is_leaf_test0 = is_leaf (Leaf "L3")
 let my_is_leaf_test1 = not (is_leaf (Node ("N2", [Leaf "L2"])))
 let my_is_leaf_test2 = not (is_leaf (Node ("+", [Leaf 3; Node ("*", [Leaf 4; Leaf 5])])))
 
@@ -42,4 +39,68 @@ let my_parse_tree_leaves_test0 = parse_tree_leaves (Node ("+", [Leaf 3; Node ("*
 let my_parse_tree_leaves_test1 = parse_tree_leaves (Leaf 3)
 let my_parse_tree_leaves_test2 = parse_tree_leaves (Node ("N1", [Leaf "L1"; (Node ("N2", [Leaf "L2"])); Leaf "L3"]))
 let my_parse_tree_leaves_test3 = parse_tree_leaves (Node ("+", [Leaf 3; Node ("*", [Leaf 4; Leaf 5]); Leaf 6]))
-let my_parse_tree_leaves_test3 = parse_tree_leaves (Node ("+", [Leaf 3; Node ("*", [Leaf 4; Leaf 5]); Leaf 6; Node ("-", [Leaf 7; Leaf 8])]))
+let my_parse_tree_leaves_test3 = parse_tree_leaves (Node ("+", [Leaf 3; Node ("*", [Leaf 4; Leaf 5]); Leaf 6; Node ("-", [Leaf 7; Leaf 8])])) *)
+
+let accept_all string = Some string
+let accept_empty_suffix = function
+   | _::_ -> None
+   | x -> Some x
+
+
+type awksub_nonterminals =
+  | Expr | Term | Lvalue | Incrop | Binop | Num
+
+let awkish_grammar =
+  (Expr,
+   function
+     | Expr ->
+         [[N Term; N Binop; N Expr];
+          [N Term]]
+     | Term ->
+     [[N Num];
+      [N Lvalue];
+      [N Incrop; N Lvalue];
+      [N Lvalue; N Incrop];
+      [T"("; N Expr; T")"]]
+     | Lvalue ->
+     [[T"$"; N Expr]]
+     | Incrop ->
+     [[T"++"];
+      [T"--"]]
+     | Binop ->
+     [[T"+"];
+      [T"-"]]
+     | Num ->
+     [[T"0"]; [T"1"]; [T"2"]; [T"3"]; [T"4"];
+      [T"5"]; [T"6"]; [T"7"]; [T"8"]; [T"9"]])
+
+
+let test0 =
+  ((make_matcher awkish_grammar accept_all ["ouch"]) = None)
+
+let test1 =
+  ((make_matcher awkish_grammar accept_all ["9"])
+   = Some [])
+
+let test2 =
+  ((make_matcher awkish_grammar accept_all ["9"; "+"; "$"; "1"; "+"])
+   = Some ["+"])
+
+let test3 =
+  ((make_matcher awkish_grammar accept_empty_suffix ["9"; "+"; "$"; "1"; "+"])
+   = None)
+
+(* This one might take a bit longer.... *)
+let test4 =
+ ((make_matcher awkish_grammar accept_all
+     ["("; "$"; "8"; ")"; "-"; "$"; "++"; "$"; "--"; "$"; "9"; "+";
+      "("; "$"; "++"; "$"; "2"; "+"; "("; "8"; ")"; "-"; "9"; ")";
+      "-"; "("; "$"; "$"; "$"; "$"; "$"; "++"; "$"; "$"; "5"; "++";
+      "++"; "--"; ")"; "-"; "++"; "$"; "$"; "("; "$"; "8"; "++"; ")";
+      "++"; "+"; "0"])
+  = Some [])
+
+ let test5 =
+  (parse_tree_leaves (Node ("+", [Leaf 3; Node ("*", [Leaf 4; Leaf 5])]))
+   = [3; 4; 5])
+
