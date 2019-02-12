00908a2b9eb6181ec4f876350171e67c8428e5ac
Christopher Tyler Ngai <ngai@lnxsrv09.seas.ucla.edu>
2019-02-11 18:03:02 -0800

    finished bettersafe

--- HW3/BetterSafeState.java
+++ HW3/BetterSafeState.java
@@ -1,3 +1,31 @@
+import java.util.concurrent.locks.ReentrantLock;
+
 class BetterSafeState implements State {
-	
+	private byte[] value;
+    private byte maxval;
+
+    //ReentrantLock owned by thread doing locking
+    private final ReentrantLock lock = new ReentrantLock();
+
+    BetterSafeState(byte[] v) { value = v; maxval = 127; }
+
+    BetterSafeState(byte[] v, byte m) { value = v; maxval = m; }
+
+    public int size() { return value.length; }
+
+    public byte[] current() { return value; }
+
+    public boolean swap(int i, int j) {
+    	lock.lock();	//acquire the lock
+
+		if (value[i] <= 0 || value[j] >= maxval) {
+		    return false;
+		}
+		value[i]--;
+		value[j]++;
+
+		lock.unlock();	//release the lock
+
+		return true;
+    }
 }
