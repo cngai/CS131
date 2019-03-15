d108b127a6149dcfc0f9ad70ffafeaea1cd21cb5
Christopher Tyler Ngai <ngai@lnxsrv09.seas.ucla.edu>
2019-03-14 23:43:57 -0700

    cleaned up test cases

--- HW6/everyNth.kt
+++ HW6/everyNth.kt
@@ -1,6 +1,12 @@
 // accepts list L and positive integer N and returns list containing every
 // Nth element of L, starting with (N-1)st element, then (2N-1)st element
-fun <T> everyNth(l: List<T>, n: Int): MutableList<T> {
+fun <T> everyNth(l: List<T>, n: Int): List<T> {
+	// make sure valid input
+	if (l.size < n) {
+		val emptyList = listOf<T>()
+		return emptyList
+	}
+
 	val myList: MutableList<T> = mutableListOf()
 	var length = l.size - 1
 	for (i in 0..length) {
@@ -13,7 +19,21 @@ fun <T> everyNth(l: List<T>, n: Int): MutableList<T> {
 }
 
 fun main() {
-	val list = listOf("1", "2", "hi", "4", "5", "bye", "7")
-	var answer = everyNth(list, 3)
-	println(answer)
+	// empty list
+	println("Test 1")
+	val list_1 = listOf<Int>()
+	println(everyNth(list_1, 2))
+	println()
+
+	// list of strings, should print out [hi, bye]
+	println("Test 2")
+	val list_2 = listOf("1", "2", "hi", "4", "5", "bye", "7")
+	println(everyNth(list_2, 3))
+	println()
+
+	// list of ints, should print out [2, 4, 6, 8]
+	println("Test 3")
+	val list_3 = listOf(1, 2, 3, 4, 5, 6, 7, 8)
+	println(everyNth(list_3, 2))
+	println()
 }
